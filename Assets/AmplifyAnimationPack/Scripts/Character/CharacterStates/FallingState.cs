47901fc9d54df9dbd4c950731d976a942b2b3858
juno11234 <cozam2@naver.com>
2025-06-07 03:33:02 +0900

    Initial commit

---
+++ Assets/AmplifyAnimationPack/Scripts/Character/CharacterStates/FallingState.cs
@@ -0,0 +1,57 @@
+﻿// Amplify Animation Pack - Third-Person Character Controller
+// Copyright (c) Amplify Creations, Lda <info@amplify.pt>
+
+using UnityEngine;
+
+
+namespace AmplifyAnimationPack
+{
+
+	public class FallingState : CharacterStateBase
+	{
+		public override void EnterState()
+		{
+			charRef.rigid.useGravity = true;
+			charRef.rigid.interpolation = RigidbodyInterpolation.Interpolate;
+			charRef.baseColl.enabled = true;
+			charRef.baseColl.radius = 0.1f;
+
+			charRef.ChangeCollider( CharacterColliderTypes.baseCol );
+			charRef.ChangeMovementMode( CharacterMovementModes.physicsBased );
+			charRef.ChangeMovementType( CharacterMovementTypes.falling );
+
+			if( prevState != null )
+			{
+				if( prevState is GroundedState )
+				{
+					charRef.isJumpingForward = ( charRef.animBeh.anim.velocity.magnitude > 0f && Mathf.Abs( charRef.animBeh.anim.velocity.y ) < 0.5f );
+					if( charRef.isJumpingForward )
+					{
+						charRef.rigid.AddForce( charRef.transf.forward * ( charRef.currLocomotionType == CharacterLocomotionTypes.walking ? 3f : 7f ) , ForceMode.Impulse );
+					}
+				}
+			}
+
+		}
+
+		public override void ExitState()
+		{
+			charRef.baseColl.radius = 0.5f;
+		}
+
+		public override void UpdateState()
+		{
+
+			if( Physics.Raycast( charRef.transf.position , -Vector3.up , 0.9f ) )
+			{
+				charRef.ChangeState<GroundedState>();
+			}
+		}
+
+		public override void UpdateStatePhysics()
+		{
+			charRef.rigid.velocity += Vector3.up * Physics.gravity.y * charRef.fallMultiplier * Time.deltaTime;
+		}
+
+	}
+}

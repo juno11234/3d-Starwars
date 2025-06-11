deeb96639de5ed67c74a730f8c7d0ce931e865d2
kjh7568 <rwrwg159@gmail.com>
2025-06-11 16:07:48 +0900

    활공시스템 제작준비

---
+++ Assets/2_Scripts/Player/PlayerState/FlyState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyState : IPlayerState
+{
+    public void Enter() { }
+    public void Input() { }
+    public void UpdateLogic() { }
+    public void Exit() { }
+}
